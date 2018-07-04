bdccb010f575f314844031cec8e42c3b9b4ff675
Sehyeon Youn <d.sehyeon@gmail.com>
2018-07-04 11:27:22 +0900

    config/urls.py static url write

--- app/config/settings.py
+++ app/config/settings.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
