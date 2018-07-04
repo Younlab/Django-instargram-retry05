293f2d750ef8d144e677833cc6f0191b932b7f93
Sehyeon Youn <d.sehyeon@gmail.com>
2018-07-04 12:57:40 +0900

    Post model create

--- app/posts/models.py
+++ app/posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from config import settings
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
+    image = models.ImageField(upload_to='post_image', blank=True)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
